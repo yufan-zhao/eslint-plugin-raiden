fcd08dcb7b540eadef215cbaf69d699eb7dd172a
Yufan <yufan.zhao@hotmail.com>
2021-12-28 16:12:45 +0800

    更新规则

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,50 @@
 "use strict";
 
 module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:eslint-plugin/recommended",
-    "plugin:node/recommended",
-  ],
-  env: {
-    node: true,
-  },
-  overrides: [
-    {
-      files: ["tests/**/*.js"],
-      env: { mocha: true },
-    },
-  ],
-  rules:
-  {
-    "indent": [
-      1,
-      "tab",
-      {"SwitchCase": 1}
-    ]
-  }
+	root: true,
+	extends: [
+		"eslint:recommended",
+		"plugin:eslint-plugin/recommended",
+		"plugin:node/recommended",
+	],
+	env: {
+		node: true,
+	},
+	rules: {
+		// Possible Problems
+		"no-unused-vars": [
+			1,
+			{
+				vars: "local",
+				args: "none",
+				ignoreRestSiblings: false,
+				argsIgnorePattern: "^_",
+				caughtErrors: "none",
+			},
+		],
+		// Suggestions
+		quotes: [2, "double", { allowTemplateLiterals: true }],
+		// Layout & Formatting
+		indent: [
+			2,
+			"tab",
+			{
+				SwitchCase: 4,
+			},
+		],
+		"brace-style": [
+			2,
+			"allman",
+			{
+				allowSingleLine: true,
+			},
+		],
+		semi: [2, "always"],
+	},
+	overrides: [
+		{
+			files: ["tests/**/*.js"],
+			env: { mocha: true },
+		},
+	],
 };
